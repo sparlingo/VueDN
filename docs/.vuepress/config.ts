5896e8a296351ff87e837f96c6e476be04715eb3
Kevin Sparling <kevinsparling@gmail.com>
2023-12-27 17:01:28 +0800

    initial commit

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,85 @@
+import { defaultTheme, defineUserConfig } from "vuepress";
+import { registerComponentsPlugin } from "@vuepress/plugin-register-components";
+import { searchPlugin } from "@vuepress/plugin-search";
+import { getDirname, path } from "@vuepress/utils";
+import { glob } from "glob";
+
+let songFiles = glob
+  .sync("docs/songs/**/*.md")
+  .map((f) => f.replace("docs", "").replace("index.md", ""));
+
+import { description } from "../../package.json";
+
+const __dirname = getDirname(import.meta.url);
+
+export default defineUserConfig({
+  lang: "en-US",
+  // Global title in HTML <head>.
+  // If page has title (in frontmatter) or h1 then: <page title/h1> | <global title>
+  // e.g <title>Vuepress-DecapCMS-Netlify | VueDN</title>
+  title: "Be The Best Man",
+  // Global description in in HTML <head>.
+  // If page has description (in frontmatter) then: <global description is replaced by <page description>
+  // <meta name="description" content="...">
+  description: description,
+  head: [
+    [
+      "script",
+      {
+        src: "https://identity.netlify.com/v1/netlify-identity-widget.js",
+      },
+    ],
+  ],
+
+  // theme and its config
+  theme: defaultTheme({
+    logo: "goals.png",
+    notFound: ["There's nothing here. If you're looking for DecapCMS, manually enter `/admin` to the root site path to navigate directly to it."],
+    navbar: [
+      {
+        text: "Songs",
+        // notice the trailing / (for the automatic next and prev links based on the sidebar)
+        link: "/songs/",
+      },
+      {
+        text: "Using this template",
+        link: "/template/",
+      },
+      {
+        text: "GitHub",
+        link: "https://github.com/NdagiStanley/VueDN",
+      },
+    ],
+    // notice there's a difference between /songs and /songs/
+    // We have the /songs to enable this sidebar for /songs and /songs/ paths
+    sidebar: {
+      "/songs": [
+        {
+          text: "Songs",
+          children: songFiles,
+        },
+      ],
+    },
+  }),
+
+  // Replace footer
+  alias: {
+    "@theme/HomeFooter.vue": path.resolve(
+      __dirname,
+      "./components/MyHomeFooter.vue"
+    ),
+  },
+
+  // plugin
+  plugins: [
+    registerComponentsPlugin({
+      // options
+      // Absolute path to the components directory
+      componentsDir: path.resolve(__dirname, "./components"),
+    }),
+    searchPlugin({
+      // options
+      // Default shortcut is key '/'
+    }),
+  ],
+});
---
+++ docs/songs/index.md
@@ -0,0 +1,45 @@
+---
+contributors: false
+lastUpdated: false
+---
+
+# All Goals
+
+Here is where I list all of the things that I want to do for the year.
+
+::: details
+
+`songs` here (the directory and contents) is used as an example of document directory that will be a collection in Decap CMS. The `index.md` files are within directories named in the format "YYYY-MM-DD".
+
+Update as needed.
+
+```txt:no-line-numbers
+. (Root)
+├── docs
+│   ├── .vuepress
+│   │   │   ...
+│   │   └── config.ts
+│   ├── README.md
+│   └── songs
+│       ├── 2023-04-10
+│       │   └── index.md
+│       ├── 2023-04-17
+│       │   └── index.md
+│       │   ...
+│       └── index.md (Current file)
+│   ...
+├── README.md
+└── yarn.lock
+```
+
+:::
+
+The pages have a standard format.
+
+## Format
+
+- Title
+- Links
+  - Youtube
+  - Spotify
+- Lyrics (with translations if available)
